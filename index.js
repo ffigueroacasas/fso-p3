84f225653f9870a039230857d23588e2f845bb1b
Francisco Figueroa Casas <franciscofigueroacasas@gmail.com>
2025-01-07 16:21:31 -0300

    fix port definition

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.post('/api/persons', (req, res) => {
   return res.status(201).json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
