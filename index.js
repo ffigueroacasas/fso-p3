c9a56de4c4e1331e778c3b908fa9efc92c76ee11
Francisco Figueroa Casas <franciscofigueroacasas@gmail.com>
2025-01-07 16:56:59 -0300

    add static middleware

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import cors from 'cors'
 const app = express()
 
 app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 morgan.token("body", (req, res) => JSON.stringify(req.body) === "{}" ? null : ` - ${JSON.stringify(req.body)}`)
 app.use(morgan(":method :url :status :res[content-length] :response-time ms :body"))
